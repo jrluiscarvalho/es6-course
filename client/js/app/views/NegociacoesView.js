a3c97165e5c40fb1488c8440a6bed923b72992d0
luis <jrluiscarvalho@gmail.com>
2017-04-25 01:59:44 -0300

    extends

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,10 +1,10 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
-    constructor(element){
-        this._element = element;
+    constructor(elemento){
+        super(elemento);
     }
 
-    _template(model){
+    _template(model){   
         return `
         <table class="table table-hover table-bordered">
         <thead>
@@ -39,8 +39,5 @@ class NegociacoesView {
         `;
     }
 
-    update(model){
-        this._element.innerHTML = this._template(model);
-    }
 }
 
