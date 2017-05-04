ba9fc8d637bf27aec8305805e1ad455f7e0d37a2
luis <jrluiscarvalho@gmail.com>
2017-05-04 02:25:01 -0300

    changes

---
+++ client/js/app/services/ProxyFactory.js
@@ -0,0 +1,16 @@
+class ProxyFactory {
+    static createProxy(objeto, props, acao){
+        return new Proxy(new ListaNegociacoes(), {
+            get(target, prop, receiver){
+                if(props.includes(prop) && typeof(target[prop]) == typeof(Function)){
+                    return function(){
+                        console.log(`interceptando ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        return acao(target);
+                    }
+                }
+                return Reflect.get(target, prop, receiver);
+            }
+        });
+    }
+}
