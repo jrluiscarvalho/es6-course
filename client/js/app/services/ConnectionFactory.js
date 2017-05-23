a3782f8b65552fd454a089c6a47598860d2169a6
luis <jrluiscarvalho@gmail.com>
2017-05-23 00:21:33 -0300

    create module pattern for db connection

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,5 +1,54 @@
-class ConnectionFactory{
-    static getConnection(){
+var ConnectionFactory = (function (){
+
+
+    var stores = ['negociacoes', 'clientes'];
+    var version = 4;
+    var dbName = 'aluraframe';
+
+    var connection = null;
+
+    return class ConnectionFactory{
         
+        constructor(){
+
+            throw new Error('Não é possivel criar instâncias de ConnectionFactory');
+        }
+
+        static getConnection(){
+            return new Promise((resolve, reject) => {
+                
+                let openRequest = window.indexedDB.open('aluraframe', 4);
+
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                };
+
+                openRequest.onsuccess = e => {
+                    if(!connection) {
+                        connection = e.target.result;
+                        connection.close = function(){
+                            throw new Error('Voce nao pode fechar diretamente uma conexao');
+                        }
+                    }
+                    resolve(connection);
+                };
+
+                openRequest.onerror = e => {
+                    console.log(e.targe.error);
+                    reject(e.target.error.name);
+                };
+
+            });
+        }
+
+        static _createStores(connection){
+            stores.forEach(store =>{
+                if(connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store);
+                }
+                connection.createObjectStore(store, {autoIncrement: true});
+
+            });
+        }
     }
-}
+})();
