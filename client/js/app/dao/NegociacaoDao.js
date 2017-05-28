f553456ebecea158028d6d43b7057eaff763b5b3
luis <jrluiscarvalho@gmail.com>
2017-05-28 20:40:17 -0300

    new dao

---
+++ client/js/app/dao/NegociacaoDao.js
@@ -0,0 +1,32 @@
+class Negociacao {
+    constructor(connection){
+
+        this._connection = connection;
+        this._store = 'negociacoes';
+
+    }
+
+    adiciona(negociacao){
+        return new Promise((resolve, reject) => {
+
+            let request = 
+                this._connection
+                .transaction([this._store], 'readwrite')
+                .objectStore(this._store)
+                .store.add(negociacao);
+
+            request.onsuccess = e => {
+                
+                resolve();
+            };
+
+            request.onerror = e => {
+
+                console.log(e.target.error);
+                reject('Não foi possivel a negociacao');
+                
+            };
+
+        });
+    }
+}
